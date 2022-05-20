9c783ad5e878a2b8d113213cb92beac88dd2e7ba
Damola Adediran <tylher123@gmail.com>
2022-05-20 17:35:55 +0100

    Fix eslint linters

--- src/__test__/components_test/components.test.js
+++ src/__test__/components_test/components.test.js
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
 import { BrowserRouter as Router } from 'react-router-dom';
 import store from '../../redux/configureStore';
-import Continents from '../../components/continents/continents';
+import Continents from '../../components/continents/Continents';
 import Countries from '../../components/countries/countries';
 import App from '../../App';
 
